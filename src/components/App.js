cc829f5f0bcf7f865beaba58a027ee754f46278b
oloomoses <olooine@gmail.com>
2021-04-08 19:26:20 +0300

    update the navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import '../App.css';
-import NavBar from './NavBar';
 import BooksList from '../containers/BooksList';
 import BooksForm from '../containers/BooksForm';
 
 const App = () => (
   <div className="wrapper">
-    <NavBar />
     <div className="container is-fluid">
       <BooksList />
       <BooksForm />
--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { IconContext } from 'react-icons';
+import { FaUserCircle } from 'react-icons/fa';
 
 const CategoryFilter = ({ handleFilter }) => {
   const categories = ['All', 'Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
@@ -10,20 +12,35 @@ const CategoryFilter = ({ handleFilter }) => {
   };
 
   return (
-    <div className="field has-addons">
-      <div className="control">
-        <div className="button">Filter :</div>
+
+    <nav className="navbar navbar-custom box" role="navigation" aria-label="main navigation">
+
+      <div className="navbar-brand">
+        <div className="navbar-item">
+          <div className="brand Bookstore-CMS">Bookstore CMS</div>
+        </div>
       </div>
-      <p className="control is-expanded">
-        <span className="select is-fullwidth">
+
+      <div className="navbar-menu">
+        <div className="navbar-item BOOKS">BOOKS</div>
+        <div className="navbar-item CATEGORIES">
+          <div className="control">
+            <div className="">Categories : </div>
+          </div>
           <select name="filter" id="filter" onChange={filterByCat}>
             {categories.map((cat) => (
               <option key={cat} value={cat}>{cat}</option>
             ))}
           </select>
-        </span>
-      </p>
-    </div>
+        </div>
+      </div>
+
+      <div className="navbar-end">
+        <IconContext.Provider value={{ size: '30px', color: '#379cf6' }}>
+          <FaUserCircle />
+        </IconContext.Provider>
+      </div>
+    </nav>
   );
 };
 
